d2e6b0ee9afc39c86ee78b3171d991af0ec0a947
Embun Code <hellowasis@gmail.com>
2025-11-26 14:14:17 +0700

    update compare password login and hidden .env

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,23 +1,51 @@
+import bcrypt from 'bcrypt';
 import { findUserByEmail } from '../repositories/user.repository';
 import { generateToken, verifyToken } from '../utils/jwt';
 import { JwtPayload } from 'jsonwebtoken';
 
 export const loginUser = async (email: string, password: string) => {
-    const user = await findUserByEmail(email);
-    if (!user || user.password !== password) {
-        throw new Error('Invalid email or password');
-    }
-    const accessToken = generateToken({ id: user.id, email: user.email }, { expiresIn: '15m' });
-    const refreshToken = generateToken({ id: user.id, email: user.email }, { expiresIn: '7d' });
-    return { accessToken, refreshToken };
+  // cek user berdasarkan email
+  const user = await findUserByEmail(email);
+  if (!user) {
+    throw new Error('Invalid email on data base');
+  }
+
+  console.log("User found:", user);
+  console.log("Password provided:", password);
+  console.log("Hashed password:", user.password);
+  
+  // cek password hash
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw new Error('Invalid email or password');
+  }
+
+  // generate tokens
+  const accessToken = generateToken(
+    { id: user.id, email: user.email },
+    { expiresIn: '15m' }
+  );
+
+  const refreshToken = generateToken(
+    { id: user.id, email: user.email },
+    { expiresIn: '7d' }
+  );
+
+  return { accessToken, refreshToken };
 };
 
 
 export const refreshAccessToken = async (refreshToken: string) => {
   try {
     const decoded = verifyToken(refreshToken) as JwtPayload;
-    return generateToken({ id: decoded.id, email: decoded.email }, { expiresIn: '15m' });
-  } catch {
+
+    // generate access token baru
+    return generateToken(
+      { id: decoded.id, email: decoded.email },
+      { expiresIn: '15m' }
+    );
+
+  } catch (err) {
     throw new Error('Invalid refresh token');
   }
 };
